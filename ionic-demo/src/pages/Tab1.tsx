6b09889a1b3d1dd5b739bedc54f204fa51e16b03
Eric Horbinski <ehorbinski@root16.com>
2023-12-12 10:36:07 -0600

    add swiper, update tabs for better names

--- ionic-demo/src/pages/Tab1.tsx
+++ ionic-demo/src/pages/Tab1.tsx
@@ -1,22 +1,55 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
-
+import {
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import { Navigation, Mousewheel, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./Tab1.css";
+import "swiper/css/bundle";
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Find an Office</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Find Office</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <Swiper
+          pagination={{ clickable: true }}
+          mousewheel
+          modules={[Pagination, Mousewheel]}
+          loop={true}
+        >
+          <SwiperSlide>
+            <IonCard title="SLIDE 1">
+              <IonCardContent>I am slide 1</IonCardContent>
+            </IonCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <IonCard title="SLIDE 2">
+              <IonCardContent>I am slide 2</IonCardContent>
+            </IonCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <IonCard title="SLIDE 3">
+              <IonCardContent>I am slide 3</IonCardContent>
+            </IonCard>
+          </SwiperSlide>
+        </Swiper>
       </IonContent>
     </IonPage>
   );
